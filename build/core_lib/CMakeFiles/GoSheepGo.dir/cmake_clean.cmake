f180de419e2e86b07504f54fa2dea8814672e560
EthanTLee <tethanlee@gmail.com>
2021-03-01 17:43:42 -1000

    init

---
+++ build/core_lib/CMakeFiles/GoSheepGo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoSheepGo.dir/src/GoSheepGoProgram.o"
+  "libGoSheepGo.dylib"
+  "libGoSheepGo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoSheepGo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
